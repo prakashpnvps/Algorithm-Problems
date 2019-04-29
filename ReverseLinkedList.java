8febfa29aa0ee62dec4fcc73a908a15665514fa5
Prakash Polavarapu <35242687+prakashpnvps@users.noreply.github.com>
2019-04-28 23:12:52 -0400

    Add files via upload

---
+++ ReverseLinkedList.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
+
+/* Recursive solution
+public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+*/
